4a99ddb916760be5f924bd173f89bc00738aecc8
asdfsaint <asdfsaint@proton.me>
2026-06-24 10:37:41 -0700

    Added scanning S3 buckets

--- AWScanner.py
+++ AWScanner.py
@@ -3,6 +3,7 @@ from botocore.exceptions import ClientError, NoCredentialsError
 
 session = boto3.Session()
 sts = session.client("sts")
+s3 = session.client("s3")
 
 try:
     identity = sts.get_caller_identity()
@@ -12,3 +13,13 @@ except NoCredentialsError:
     print("No credentials found. Run 'aws configure' first.")
 except ClientError as e:
     print(f"AWS rejected the request: {e}")
+
+try:
+    response = s3.list_buckets()
+    for bucket in response['Buckets']:
+        print(bucket['Name'])
+except ClientError as e:
+    print (f"Failed to list buckets: {e}")
+
+
+
